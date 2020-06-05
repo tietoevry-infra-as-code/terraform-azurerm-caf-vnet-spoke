d1a164505b00e0a06fba646a11a55ae9c7f04ef5
Kumaraswamy Vithanala <64014824+kumarvit@users.noreply.github.com>
2020-06-05 21:15:34 +0530

    initial commit

---
+++ output.tf
@@ -0,0 +1,69 @@
+# Resource Group
+output "resource_group_name" {
+  description = "The name of the resource group in which resources are created"
+  value       = element(coalescelist(data.azurerm_resource_group.rgrp.*.name, azurerm_resource_group.rg.*.name, [""]), 0)
+}
+
+output "resource_group_id" {
+  description = "The id of the resource group in which resources are created"
+  value       = element(coalescelist(data.azurerm_resource_group.rgrp.*.id, azurerm_resource_group.rg.*.id, [""]), 0)
+}
+
+output "resource_group_location" {
+  description = "The location of the resource group in which resources are created"
+  value       = element(coalescelist(data.azurerm_resource_group.rgrp.*.location, azurerm_resource_group.rg.*.location, [""]), 0)
+}
+
+# Vnet and Subnets
+output "virtual_network_name" {
+  description = "The name of the virtual network"
+  value       = element(concat(azurerm_virtual_network.vnet.*.name, [""]), 0)
+}
+
+output "virtual_network_id" {
+  description = "The id of the virtual network"
+  value       = element(concat(azurerm_virtual_network.vnet.*.id, [""]), 0)
+}
+
+output "virtual_network_address_space" {
+  description = "List of address spaces that are used the virtual network."
+  value       = element(coalescelist(azurerm_virtual_network.vnet.*.address_space, [""]), 0)
+}
+
+output "subnet_ids" {
+  description = "List of IDs of subnets"
+  value       = [for s in azurerm_subnet.snet : s.id]
+}
+
+output "subnet_address_prefixes" {
+  description = "List of address prefix for subnets"
+  value       = [for s in azurerm_subnet.snet : s.address_prefix]
+}
+
+# Network Security group ids
+output "network_security_group_ids" {
+  description = "List of Network security groups and ids"
+  value       = [for n in azurerm_network_security_group.nsg : n.id]
+}
+
+# DDoS Protection Plan
+output "ddos_protection_plan_id" {
+  description = "Ddos protection plan details"
+  value       = element(concat(azurerm_network_ddos_protection_plan.ddos.*.id, [""]), 0)
+}
+
+# Network Watcher
+output "network_watcher_id" {
+  description = "ID of Network Watcher"
+  value       = element(concat(azurerm_network_watcher.nwatcher.*.id, [""]), 0)
+}
+
+output "route_table_name" {
+  description = "The name of the route table"
+  value       = azurerm_route_table.rtout.name
+}
+
+output "route_table_id" {
+  description = "The resource id of the route table"
+  value       = azurerm_route_table.rtout.id
+}
---
+++ variables.tf
@@ -0,0 +1,120 @@
+variable "create_resource_group" {
+  description = "Whether to create resource group and use it for all networking resources"
+  default     = true
+}
+
+variable "resource_group_name" {
+  description = "A container that holds related resources for an Azure solution"
+  default     = ""
+}
+
+variable "location" {
+  description = "The location/region to keep all your network resources. To get the list of all locations with table format from azure cli, run 'az account list-locations -o table'"
+  default     = ""
+}
+
+variable "project_name" {
+  description = "The name of the project."
+  default     = ""
+}
+
+variable "subscription_type" {
+  description = "Summary description of the purpose of the subscription that contains the resource. Often broken down by deployment environment type or specific workloads"
+  default     = ""
+}
+
+variable "environment" {
+  description = "The stage of the development lifecycle for the workload that the resource supports"
+  default     = ""
+}
+
+variable "is_spoke_deployed_to_same_hub_subscription" {
+  default = true
+}
+
+variable "vnet_address_space" {
+  description = "The address space to be used for the Azure virtual network."
+  default     = ["10.0.0.0/16"]
+}
+
+variable "create_ddos_plan" {
+  description = "Create an ddos plan - Default is false"
+  default     = false
+}
+
+variable "dns_servers" {
+  description = "List of dns servers to use for virtual network"
+  default     = []
+}
+
+variable "create_network_watcher" {
+  description = "Controls if Network Watcher resources should be created for the Azure subscription"
+  default     = true
+}
+
+variable "subnets" {
+  description = "For each subnet, create an object that contain fields"
+  default     = {}
+}
+
+variable "hub_virtual_network_id" {
+  description = "The id of hub virutal network"
+  default     = ""
+}
+
+variable "hub_firewall_private_ip_address" {
+  description = "The private IP of the hub virtual network firewall"
+  default     = ""
+}
+
+/*variable "service_principals" {
+  description = "List of principal ids that need access to peer to this Hub network. All service principals used to deploy spoke networks must have access to peer."
+  type        = list(string)
+  default     = []
+}*/
+
+variable "private_dns_zone_name" {
+  description = "The name of the Private DNS zone"
+  default     = null
+}
+
+variable "use_remote_gateways" {
+  description = "Controls if remote gateways can be used on the local virtual network."
+  default     = false
+}
+
+variable "hub_storage_account_id" {
+  description = "The id of hub storage id for logs storage"
+  default     = ""
+}
+
+variable "log_analytics_workspace_id" {
+  description = "Specifies the id of the Log Analytics Workspace"
+  default     = ""
+}
+
+variable "log_analytics_customer_id" {
+  description = "The Workspace (or Customer) ID for the Log Analytics Workspace."
+  default     = ""
+}
+
+variable "log_analytics_logs_retention_in_days" {
+  description = "The log analytics workspace data retention in days. Possible values range between 30 and 730."
+  default     = ""
+}
+
+variable "azure_monitor_logs_retention_in_days" {
+  description = "The Azure Monitoring data retention in days."
+  default     = ""
+}
+
+variable "nsg_diag_logs" {
+  description = "NSG Monitoring Category details for Azure Diagnostic setting"
+  default     = ["NetworkSecurityGroupEvent", "NetworkSecurityGroupRuleCounter"]
+}
+
+variable "tags" {
+  description = "A map of tags to add to all resources"
+  type        = map(string)
+  default     = {}
+}
